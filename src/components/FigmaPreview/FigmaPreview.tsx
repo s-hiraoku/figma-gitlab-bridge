2a475337c9fbf655404d4c6f62330a0a64849b13
hiraoku.shinichi <s.hiraoku@gmail.com>
2023-06-02 08:01:39 +0900

    Add loading to FigmaPreview

--- src/components/FigmaPreview/FigmaPreview.tsx
+++ src/components/FigmaPreview/FigmaPreview.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { Loading } from "@components/Loading";
+import React, { useState } from "react";
 
 type FigmaPreviewProps = {
   url: string;
@@ -9,14 +10,23 @@ export const FigmaPreview: React.FC<FigmaPreviewProps> = ({
   url,
   title = "Figma Preview",
 }) => {
+  const [loading, setLoading] = useState(true);
   return (
-    <iframe
-      title={title}
-      className="w-full h-full"
-      src={`https://www.figma.com/embed?embed_host=share&url=${encodeURIComponent(
-        url
-      )}`}
-      allowFullScreen
-    />
+    <>
+      {loading && (
+        <div className="absolute inset-0 flex items-center justify-center">
+          <Loading />
+        </div>
+      )}
+      <iframe
+        title={title}
+        className="w-full h-full"
+        src={`https://www.figma.com/embed?embed_host=share&url=${encodeURIComponent(
+          url
+        )}`}
+        onLoad={() => setLoading(false)}
+        allowFullScreen
+      />
+    </>
   );
 };
