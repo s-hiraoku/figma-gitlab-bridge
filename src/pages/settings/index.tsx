1b1ecf8a48d11f34fabfc41a26e81fca9f81433b
hiraoku.shinichi <s.hiraoku@gmail.com>
2023-07-08 19:49:26 +0900

    Add suspense and error boundary to Settings page

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -13,8 +13,7 @@ import {
   FormControl,
 } from "@mui/material";
 import { useSettings } from "@hooks/useSettings";
-import { useCallback, useEffect, useState } from "react";
-import { SETTING_KEY, findValueInSettingsByKey } from "@features/settings";
+import { Suspense, useCallback, useEffect, useState } from "react";
 import { FetchError } from "@components/FetchError";
 import { toast } from "react-toastify";
 import { useTheme } from "@mui/system";
@@ -24,6 +23,10 @@ import { FIllED_INPUT_TYPE } from "@utils/ui";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { useRouter } from "next/router";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Loading } from "@components/Loading";
+import { ErrorBoundary } from "@components/ErrorBoundary";
+import { useFigmaSettings } from "@hooks/useFigmaSettings";
+import { useGitLabSettings } from "@hooks/useGitLabSettings";
 
 const FIELD_DEFAULT_STYLE: SxProps<Theme> = { mt: 8, width: 800 };
 const FIELD_DEFAULT_TITLE_STYLE: SxProps<Theme> = {
@@ -34,14 +37,12 @@ const FIELD_DEFAULT_TITLE_STYLE: SxProps<Theme> = {
 
 export default function Settings() {
   const theme = useTheme();
-  const {
-    data: settings,
-    error,
-    isValidating,
-    mutate: revalidate,
-  } = useSettings();
+  const { mutate: revalidate } = useSettings();
   const { apiClient } = useApiClient();
   const router = useRouter();
+  const { getFigmaAccessToken, getFigmaAPIEndpoint } = useFigmaSettings();
+  const { getGitLabAPIEndpoint, getGitLabAccessToken, getGitLabProjectPath } =
+    useGitLabSettings();
 
   const [figmaApiEndpoint, setFigmaApiEndpoint] = useState<string>("");
   const [figmaAccessToken, setFigmaAccessToken] = useState<string>("");
@@ -55,25 +56,18 @@ export default function Settings() {
     useBoolean(false);
 
   useEffect(() => {
-    if (settings) {
-      setFigmaApiEndpoint(
-        findValueInSettingsByKey(settings, SETTING_KEY.figmaAPIEndpoint) ?? ""
-      );
-      setFigmaAccessToken(
-        findValueInSettingsByKey(settings, SETTING_KEY.figmaAccessToken) ?? ""
-      );
-      setGitLabProjectPath(
-        findValueInSettingsByKey(settings, SETTING_KEY.gitLabProjectPath) ?? ""
-      );
-      setGitLabApiEndpoint(
-        findValueInSettingsByKey(settings, SETTING_KEY.gitLabAPIEndpoint) ?? ""
-      );
-      setGitLabAccessToken(
-        findValueInSettingsByKey(settings, SETTING_KEY.gitLabAccessToken) ?? ""
-      );
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isValidating]);
+    setFigmaApiEndpoint(getFigmaAPIEndpoint() ?? "");
+    setFigmaAccessToken(getFigmaAccessToken() ?? "");
+    setGitLabProjectPath(getGitLabProjectPath() ?? "");
+    setGitLabApiEndpoint(getGitLabAPIEndpoint() ?? "");
+    setGitLabAccessToken(getGitLabAccessToken() ?? "");
+  }, [
+    getFigmaAPIEndpoint,
+    getFigmaAccessToken,
+    getGitLabProjectPath,
+    getGitLabAPIEndpoint,
+    getGitLabAccessToken,
+  ]);
 
   const handleFigmaApiEndpointChange = (
     event: React.FocusEvent<HTMLInputElement>
@@ -189,10 +183,6 @@ export default function Settings() {
     router.back();
   };
 
-  if (error) {
-    return <FetchError />;
-  }
-
   return (
     <>
       <Head>
@@ -230,118 +220,132 @@ export default function Settings() {
         <Box sx={{ mt: 8 }}>
           <Typography variant="h2">⚙️ Settings</Typography>
         </Box>
-        <Typography
-          variant="h6"
-          component="h2"
-          sx={{
-            ...FIELD_DEFAULT_TITLE_STYLE,
-            color: theme.palette.primary.main,
-          }}
-        >
-          Figma API Settings
-        </Typography>
-        <Box sx={{ ...FIELD_DEFAULT_STYLE, mt: 2 }}>
-          <TextField
-            fullWidth
-            label=" Figma API endpoint"
-            variant="filled"
-            value={figmaApiEndpoint}
-            onChange={handleFigmaApiEndpointChange}
-            onBlur={handleFigmaApiEndpointBlur}
-          />
-        </Box>
-        <Box sx={FIELD_DEFAULT_STYLE}>
-          <FormControl fullWidth variant="filled">
-            <InputLabel htmlFor="figma-access-token-password">
-              Figma access token
-            </InputLabel>
-            <FilledInput
-              id="figma-access-token-password"
-              fullWidth
-              type={
-                showFigmaAccessToken
-                  ? FIllED_INPUT_TYPE.text
-                  : FIllED_INPUT_TYPE.password
-              }
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowFigmaAccessTokenPassword}
-                    onMouseDown={handleMouseDownFigmaAccessTokenPassword}
-                    edge="end"
-                  >
-                    {showFigmaAccessToken ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              }
-              value={figmaAccessToken}
-              onChange={handleFigmaAccessTokenChange}
-              onBlur={handleFigmaAccessTokenBlur}
-            />
-          </FormControl>
-        </Box>
-        <Typography
-          variant="h6"
-          component="h2"
-          sx={{
-            ...FIELD_DEFAULT_TITLE_STYLE,
-            color: theme.palette.primary.main,
-          }}
-        >
-          GitLab API Settings
-        </Typography>
-        <Box sx={{ ...FIELD_DEFAULT_STYLE, mt: 2 }}>
-          <TextField
-            fullWidth
-            label=" GitLab Project Path"
-            variant="filled"
-            value={gitLabProjectPath}
-            onChange={handleGitLabProjectPathChange}
-            onBlur={handleGitLabProjectPathBlur}
-          />
-        </Box>
-        <Box sx={FIELD_DEFAULT_STYLE}>
-          <TextField
-            fullWidth
-            label=" GitLab API endpoint"
-            variant="filled"
-            value={gitLabApiEndpoint}
-            onChange={handleGitLabApiEndpointChange}
-            onBlur={handleGitLabApiEndpointBlur}
-          />
-        </Box>
-        <Box sx={FIELD_DEFAULT_STYLE}>
-          <FormControl fullWidth variant="filled">
-            <InputLabel htmlFor="gitlab-access-token-password">
-              GitLab access token
-            </InputLabel>
-            <FilledInput
-              id="gitlab-access-token-password"
-              fullWidth
-              type={
-                showGitLabAccessToken
-                  ? FIllED_INPUT_TYPE.text
-                  : FIllED_INPUT_TYPE.password
-              }
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowGitLabAccessTokenPassword}
-                    onMouseDown={handleMouseDownGitLabAccessTokenPassword}
-                    edge="end"
-                  >
-                    {showGitLabAccessToken ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              }
-              value={gitLabAccessToken}
-              onChange={handleGitLabAccessTokenChange}
-              onBlur={handleGitLabAccessTokenBlur}
-            />
-          </FormControl>
-        </Box>
+        <ErrorBoundary fallback={<FetchError />}>
+          <Typography
+            variant="h6"
+            component="h2"
+            sx={{
+              ...FIELD_DEFAULT_TITLE_STYLE,
+              color: theme.palette.primary.main,
+            }}
+          >
+            Figma API Settings
+          </Typography>
+          <Suspense fallback={<Loading />}>
+            <Box sx={{ ...FIELD_DEFAULT_STYLE, mt: 2 }}>
+              <TextField
+                fullWidth
+                label=" Figma API endpoint"
+                variant="filled"
+                value={figmaApiEndpoint}
+                onChange={handleFigmaApiEndpointChange}
+                onBlur={handleFigmaApiEndpointBlur}
+              />
+            </Box>
+            <Box sx={FIELD_DEFAULT_STYLE}>
+              <FormControl fullWidth variant="filled">
+                <InputLabel htmlFor="figma-access-token-password">
+                  Figma access token
+                </InputLabel>
+                <FilledInput
+                  id="figma-access-token-password"
+                  fullWidth
+                  type={
+                    showFigmaAccessToken
+                      ? FIllED_INPUT_TYPE.text
+                      : FIllED_INPUT_TYPE.password
+                  }
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowFigmaAccessTokenPassword}
+                        onMouseDown={handleMouseDownFigmaAccessTokenPassword}
+                        edge="end"
+                      >
+                        {showFigmaAccessToken ? (
+                          <VisibilityOff />
+                        ) : (
+                          <Visibility />
+                        )}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  value={figmaAccessToken}
+                  onChange={handleFigmaAccessTokenChange}
+                  onBlur={handleFigmaAccessTokenBlur}
+                />
+              </FormControl>
+            </Box>
+          </Suspense>
+          <Typography
+            variant="h6"
+            component="h2"
+            sx={{
+              ...FIELD_DEFAULT_TITLE_STYLE,
+              color: theme.palette.primary.main,
+            }}
+          >
+            GitLab API Settings
+          </Typography>
+          <Suspense fallback={<Loading />}>
+            <Box sx={{ ...FIELD_DEFAULT_STYLE, mt: 2 }}>
+              <TextField
+                fullWidth
+                label=" GitLab Project Path"
+                variant="filled"
+                value={gitLabProjectPath}
+                onChange={handleGitLabProjectPathChange}
+                onBlur={handleGitLabProjectPathBlur}
+              />
+            </Box>
+            <Box sx={FIELD_DEFAULT_STYLE}>
+              <TextField
+                fullWidth
+                label=" GitLab API endpoint"
+                variant="filled"
+                value={gitLabApiEndpoint}
+                onChange={handleGitLabApiEndpointChange}
+                onBlur={handleGitLabApiEndpointBlur}
+              />
+            </Box>
+            <Box sx={FIELD_DEFAULT_STYLE}>
+              <FormControl fullWidth variant="filled">
+                <InputLabel htmlFor="gitlab-access-token-password">
+                  GitLab access token
+                </InputLabel>
+                <FilledInput
+                  id="gitlab-access-token-password"
+                  fullWidth
+                  type={
+                    showGitLabAccessToken
+                      ? FIllED_INPUT_TYPE.text
+                      : FIllED_INPUT_TYPE.password
+                  }
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowGitLabAccessTokenPassword}
+                        onMouseDown={handleMouseDownGitLabAccessTokenPassword}
+                        edge="end"
+                      >
+                        {showGitLabAccessToken ? (
+                          <VisibilityOff />
+                        ) : (
+                          <Visibility />
+                        )}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  value={gitLabAccessToken}
+                  onChange={handleGitLabAccessTokenChange}
+                  onBlur={handleGitLabAccessTokenBlur}
+                />
+              </FormControl>
+            </Box>
+          </Suspense>
+        </ErrorBoundary>
       </Box>
     </>
   );
