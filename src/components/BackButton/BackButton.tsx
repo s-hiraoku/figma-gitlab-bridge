4e4c8a84cf2f00b4d38e656722e6fb79e27a3303
hiraoku.shinichi <s.hiraoku@gmail.com>
2023-08-01 08:02:54 +0900

    Add BackButton component

---
+++ src/components/BackButton/BackButton.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+
+type BackButtonProps = {
+  top?: number;
+  left?: number;
+  onClickBack(): void;
+};
+
+export const BackButton: React.FC<BackButtonProps> = ({
+  top = 20,
+  left = 20,
+  onClickBack,
+}) => {
+  return (
+    <Button
+      sx={{
+        position: "absolute",
+        top,
+        left,
+        fontSize: "1rem",
+      }}
+      onClick={onClickBack}
+      color="secondary"
+      startIcon={<ArrowBackIcon />}
+    >
+      Back
+    </Button>
+  );
+};
